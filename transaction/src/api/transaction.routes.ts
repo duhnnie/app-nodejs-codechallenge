e48572e6a77e4dfe47ba1358f8d36a0820ce8652
Daniel Canedo <dnl.canedo@gmail.com>
2024-04-02 17:18:27 -0400

    YP-4 Create dummy endpoint for getting a transaction

--- transaction/src/api/transaction.routes.ts
+++ transaction/src/api/transaction.routes.ts
@@ -2,6 +2,10 @@ import express, { Request, Response } from "express"
 
 const router = express.Router();
 
+router.get("/:id", (req: Request, res: Response) => {
+  res.status(200).json({ id: req.params.id })
+})
+
 router.post("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Transaction created" })
 })
